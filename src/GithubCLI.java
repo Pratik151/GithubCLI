ddeb24d520000600de4108eb406afa7df5370f15
PRATIK <pratikpatel15133@gmail.com>
2015-07-26 19:15:51 +0530

    Added Files List

--- src/GithubCLI.java
+++ src/GithubCLI.java
@@ -10,30 +10,34 @@ import java.util.ArrayList;
 
 public class GithubCLI {
 
+    private String Github_Api = "https://api.github.com/";
+
     public static void main(String[] a) throws JSONException
     {
-        Files("Pratik151/GithubCLI");
+        GithubCLI cli = new GithubCLI();
+        cli.Files("Pratik151/GithubCLI");
+        cli.Repos("Pratik151");
 
 
     }
-    public static void Repos()throws JSONException
+    public void Repos(String name)throws JSONException
     {
-        JSONArray jarr = Modules.JArr("https://api.github.com/users/Pratik151/repos");
+        JSONArray jarr = Modules.JArr(Github_Api+"users/"+name+"/repos");
         ArrayList<String> al = Modules.Repos(jarr);
         for (String anAl : al) {
             System.out.println(anAl);
         }
     }
 
-    public static void Files(String name)throws JSONException
+    public void Files(String name)throws JSONException
     {
-        String url="https://api.github.com/repos/"+name+"/branches/master";
+        String url=Github_Api+"repos/"+name+"/branches/master";
         JSONObject jdata = Modules.JObj(url);
-        JSONObject sha = jdata.getJSONObject("commit");
-       url = sha.getJSONObject("commit").getJSONObject("tree").getString("url");
+        jdata = jdata.getJSONObject("commit");
+       url = jdata.getJSONObject("commit").getJSONObject("tree").getString("url");
         url = url+"?recursive=1";
         jdata = Modules.JObj(url);
-        JSONArray jarr =  jdata.getJSONArray("tree");
+        JSONArray jarr = jdata.getJSONArray("tree");
         for(int i =0;i<jarr.length();i++)
         {
             System.out.println(jarr.getJSONObject(i).getString("path"));
